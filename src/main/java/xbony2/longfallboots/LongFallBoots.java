77cff9a2f12358f63450ff0624b7f4ddb8213e9f
Eric Schneider <xbony22@gmail.com>
2020-02-07 19:30:44 -0500

    Ported to 1.15.1. My grade is very broken. The solution?
    Restarting my computer x.x
    
    Also this commit is mostly "cleanup" of the code. I was debugging this
    for a long time.

--- src/main/java/xbony2/longfallboots/LongFallBoots.java
+++ src/main/java/xbony2/longfallboots/LongFallBoots.java
@@ -1,5 +1,7 @@
 package xbony2.longfallboots;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
@@ -18,59 +20,60 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import javax.annotation.ParametersAreNonnullByDefault;
-
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 @Mod(LongFallBoots.LONGFALLBOOTS)
 public final class LongFallBoots {
 	public static final String LONGFALLBOOTS = "longfallboots";
 
-	public LongFallBoots() {
+	public LongFallBoots(){
 		final DeferredRegister<Item> deferredRegister = new DeferredRegister<>(ForgeRegistries.ITEMS, LONGFALLBOOTS);
-		final RegistryObject<ArmorItem> longFallBoots = deferredRegister.register(LONGFALLBOOTS, () ->
-			new ArmorItem(new IArmorMaterial() {
-				@Override
-				public int getDurability(final EquipmentSlotType slot) {
-					return slot == EquipmentSlotType.FEET ? 66 : 0;
-				}
+		
+		final RegistryObject<ArmorItem> longFallBoots = deferredRegister.register(LONGFALLBOOTS, () -> new ArmorItem(new IArmorMaterial(){
+			@Override
+			public int getDurability(final EquipmentSlotType slot){
+				return slot == EquipmentSlotType.FEET ? 66 : 0;
+			}
 
-				@Override
-				public int getDamageReductionAmount(final EquipmentSlotType slot) {
-					return slot == EquipmentSlotType.FEET ? 3 : 0;
-				}
+			@Override
+			public int getDamageReductionAmount(final EquipmentSlotType slot){
+				return slot == EquipmentSlotType.FEET ? 3 : 0;
+			}
 
-				@Override
-				public int getEnchantability() {
-					return 10;
-				}
+			@Override
+			public int getEnchantability(){
+				return 10;
+			}
 
-				@Override
-				public SoundEvent getSoundEvent() {
-					return SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
-				}
+			@Override
+			public SoundEvent getSoundEvent(){
+				return SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
+			}
 
-				@Override
-				public Ingredient getRepairMaterial() {
-					return Ingredient.EMPTY;
-				}
+			@Override
+			public Ingredient getRepairMaterial(){
+				return Ingredient.EMPTY;
+			}
 
-				@Override
-				public String getName() {
-					return LONGFALLBOOTS + ':' + LONGFALLBOOTS;
-				}
+			@Override
+			public String getName(){
+				return LONGFALLBOOTS + ':' + LONGFALLBOOTS;
+			}
 
-				@Override
-				public float getToughness() {
-					return 3.0F;
-				}
-			}, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)));
+			@Override
+			public float getToughness(){
+				return 2.0F;
+			}
+		}, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)));
+
 		deferredRegister.register(FMLJavaModLoadingContext.get().getModEventBus());
+
 		MinecraftForge.EVENT_BUS.<LivingFallEvent>addListener(e -> {
 			final ItemStack boots = e.getEntityLiving().getItemStackFromSlot(EquipmentSlotType.FEET);
-			if (longFallBoots.orElseThrow(IllegalStateException::new).equals(boots.getItem())) {
+			
+			if(longFallBoots.orElseThrow(IllegalStateException::new).equals(boots.getItem()))
 				e.setDamageMultiplier(0.0F);
-			}
+			
 		});
 	}
 }
