9f55e482fb713d850ec7836ae1f56720d691e6e2
Eric Schneider <xbony22@gmail.com>
2021-12-18 15:42:14 -0500

    update to 1.17

--- src/main/java/xbony2/longfallboots/LongFallBoots.java
+++ src/main/java/xbony2/longfallboots/LongFallBoots.java
@@ -2,22 +2,22 @@ package xbony2.longfallboots;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.IArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -30,29 +30,29 @@ public final class LongFallBoots {
 	public LongFallBoots(){
 		final DeferredRegister<Item> deferredRegister = DeferredRegister.create(ForgeRegistries.ITEMS, LONGFALLBOOTS);
 		
-		final RegistryObject<ArmorItem> longFallBoots = deferredRegister.register(LONGFALLBOOTS, () -> new ArmorItem(new IArmorMaterial(){
+		final RegistryObject<ArmorItem> longFallBoots = deferredRegister.register(LONGFALLBOOTS, () -> new ArmorItem(new ArmorMaterial(){
 			@Override
-			public int getDurability(final EquipmentSlotType slot){
-				return slot == EquipmentSlotType.FEET ? 429 : 0;
+			public int getDurabilityForSlot(final EquipmentSlot slot){
+				return slot == EquipmentSlot.FEET ? 429 : 0;
 			}
 
 			@Override
-			public int getDamageReductionAmount(final EquipmentSlotType slot){
-				return slot == EquipmentSlotType.FEET ? 3 : 0;
+			public int getDefenseForSlot(final EquipmentSlot slot){
+				return slot == EquipmentSlot.FEET ? 3 : 0;
 			}
 
 			@Override
-			public int getEnchantability(){
+			public int getEnchantmentValue(){
 				return 10;
 			}
 
 			@Override
-			public SoundEvent getSoundEvent(){
-				return SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
+			public SoundEvent getEquipSound(){
+				return SoundEvents.ARMOR_EQUIP_GENERIC;
 			}
 
 			@Override
-			public Ingredient getRepairMaterial(){
+			public Ingredient getRepairIngredient(){
 				return Ingredient.EMPTY;
 			}
 
@@ -70,12 +70,12 @@ public final class LongFallBoots {
 			public float getKnockbackResistance(){ // all armors have 0 except Netherite (0.1)
 				return 0;
 			}
-		}, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)));
+		}, EquipmentSlot.FEET, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
 
 		deferredRegister.register(FMLJavaModLoadingContext.get().getModEventBus());
 
 		MinecraftForge.EVENT_BUS.<LivingFallEvent>addListener(EventPriority.LOW, e -> { // Going to test the event priority, there's some mod with a conflicting event I believe
-			final ItemStack boots = e.getEntityLiving().getItemStackFromSlot(EquipmentSlotType.FEET);
+			final ItemStack boots = e.getEntityLiving().getItemBySlot(EquipmentSlot.FEET);
 			
 			if(longFallBoots.orElseThrow(IllegalStateException::new).equals(boots.getItem()))
 				e.setDamageMultiplier(0.0F);
